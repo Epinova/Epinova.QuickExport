585c92fdd2d8780dfce13261e571e1e6760aa5a4
Espen Lønnkvist <espen.lonnkvist@epinova.no>
2017-08-16 14:54:06 +0200

    Upped version to 10.x to align with Episerver

--- src/Epinova.QuickExport/Properties/AssemblyInfo.cs
+++ src/Epinova.QuickExport/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Epinova.QuickExport")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Epinova.QuickExport for Episerver 10")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Epinova AS")]
 [assembly: AssemblyProduct("Epinova.QuickExport")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("10.0.0.0")]
+[assembly: AssemblyFileVersion("10.0.0.0")]
