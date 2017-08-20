94f48c208a057938ea9431b4c7ce36281f27a206
Espen Lønnkvist <espen.lonnkvist@epinova.no>
2017-08-21 00:46:23 +0200

    Added action for handling of uploaded files

--- src/Epinova.QuickExport/Controllers/QuickExportController.cs
+++ src/Epinova.QuickExport/Controllers/QuickExportController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Web;
 using EPiServer.Enterprise;
 using System.Web.Mvc;
+using EPiServer.Cms.Shell.UI.Controllers.Internal;
 using EPiServer.Core;
 using EPiServer.Core.Transfer;
 using EPiServer.Enterprise.Internal;
 using EPiServer.Security;
+using EPiServer.Shell.Web.Mvc;
 using IOFile = System.IO.File;
 
 namespace Epinova.QuickExport.Controllers
@@ -13,8 +18,14 @@ namespace Epinova.QuickExport.Controllers
     [Authorize(Roles = "WebAdmins,Administrators")]
     public class QuickExportController : Controller
     {
+        private readonly IDataImporter _dataImporter;
         private const string FileName = "ExportedFile.episerverdata";
 
+
+        public QuickExportController(IDataImporter dataImporter)
+        {
+            _dataImporter = dataImporter;
+        }
 
         public FileResult Download(int id = 0)
         {
@@ -32,6 +43,62 @@ namespace Epinova.QuickExport.Controllers
             catch
             {
                 return null;
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Upload(int id = 0)
+        {
+            var files = GetValidFiles(Request.Files).ToArray();
+
+            if (files.Length == 0)
+                throw new Exception("No valid files supplied");
+
+            List<UploadFileResult> results = new List<UploadFileResult>();
+
+            var options = new ImportOptions
+            {
+                KeepIdentity = false,
+                //ValidateDestination = true,
+                //EnsureContentNameUniqueness = true
+            };
+
+            ContentReference destination = new ContentReference(id);
+
+            for (var i = 0; i < files.Length; i++)
+            {
+                var file = files[i];
+
+                try
+                {
+                    _dataImporter.Import(file.InputStream, destination, options);
+                }
+                catch (Exception ex)
+                {
+                    results.Add(new UploadFileResult
+                    {
+                        Index = i,
+                        FileName = file.FileName,
+                        ErrorMessage = ex.Message
+                    });
+
+                    _dataImporter.Status.Log.Error(ex.Message, ex);
+                }
+            }
+
+            return this.JsonData(results);
+        }
+
+        private IEnumerable<HttpPostedFileBase> GetValidFiles(HttpFileCollectionBase files)
+        {
+            if (files == null || files.Count == 0)
+                yield break;
+
+            for (var i = 0; i < files.Count; i++)
+            {
+                if (files[i] != null && files[i].ContentLength > 0)
+                    yield return files[i];
             }
         }
 
