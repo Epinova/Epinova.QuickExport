5141654a70b19bfe4ccf5b59ad721666536c87b7
Espen Lønnkvist <lonnkvist@gmail.com>
2017-08-22 13:04:35 +0200

    Added import. Closes #2 (#3)
    * Added 7zip cmd util
    
    * Added uncompressed client resources and creation of zip on build
    
    * Added EPiServer.CMS.UI.Core nuget
    
    * Adjust nuspec dependencies to only include UI Core
    
    * Added maxRequestLength config to transform
    
    * Updated resouce texts
    
    * Added action for handling of uploaded files
    
    * Update  zip-module
    
    * Remove dead code
    
    * Update doc for import

---
+++ src/Epinova.QuickExport/modules/_protected/QuickExport/1.0.0.0/ClientResources/Scripts/ExportCommand.js
@@ -0,0 +1,49 @@
+﻿define([
+    "dojo/topic",
+    "dojo/_base/declare",
+    "epi/dependency",
+    "epi/shell/command/_Command",
+    "epi/shell/widget/dialog/Alert",
+    "epi/i18n!epi/nls/epinovaquickexport.widget"
+], function (
+    topic,
+    declare,
+    dependency,
+    _Command,
+    Alert,
+    translator
+) {
+    return declare([_Command], {
+        name: 'export',
+        label: translator.export.button.label,
+        tooltip: translator.export.button.tooltip,
+        iconClass: 'epi-iconDownload',
+        canExecute: true,
+
+        _execute: function () {
+            dojo.rawXhrPost({
+                url: '/QuickExport/Export',
+                handleAs: 'json',
+                headers: { 'Content-Type': 'application/json' },
+                timeout: 60000,
+                postData: dojo.toJson({ 'id': this.model.contentLink }),
+                load: function (data) {
+                    if (!!data && !!data.success) {
+                        window.location = '/QuickExport/Download?id=' + data.id;
+                    } else {
+                        new Alert({
+                            title: translator.export.dialog.title,
+                            heading: translator.errors.generic
+                        }).show();
+                    }
+                },
+                error: function (error) {
+                    new Alert({
+                        title: translator.export.dialog.title,
+                        heading: translator.errors.download + ': ' + error
+                    }).show();
+                }
+            });
+        }
+    });
+});
