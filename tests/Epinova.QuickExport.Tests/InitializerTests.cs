f68f6f6b389833c94ccb164431ea984014a09cd4
Espen Lonnkvist <espen.lonnkvist@epinova.no>
2019-04-23 15:11:58 +0200

    Add unit tests

---
+++ tests/Epinova.QuickExport.Tests/InitializerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Routing;
+using Xunit;
+
+namespace Epinova.QuickExport.Tests
+{
+    public class InitializerTests
+    {
+        [Fact]
+        public void Initialize_AddsRoute()
+        {
+            var sut = new Initializer();
+            sut.Initialize(null);
+
+            Assert.True(RouteTable.Routes.AsQueryable().Cast<Route>().Any(r => r.Url == "QuickExport/{action}"));
+        }
+    }
+}
